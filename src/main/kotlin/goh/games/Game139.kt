66a222decda3390220b4ed4a66a07f11de6b31fe
Liarr <liarr@foxmail.com>
2021-05-20 19:11:27 +0800

    Fix: CP 修改 Game 139 素材路径后的脚本修正

--- src/main/kotlin/goh/games/Game139.kt
+++ src/main/kotlin/goh/games/Game139.kt
@@ -18,8 +18,8 @@ class Game139(apk: String) : Game(apk) {
     override fun replaceResource(loginImage: String?, loadingImage: String?, logoImage: String?, splashImage: String?) {
         val gameLoginImgPath = decompileDir + File.separator + "assets" + File.separator + "game" + File.separator + "resource" + File.separator + "loginres" + File.separator + "login_bg_f_44ae01ef.jpg"
         val gameSplashImgPath = decompileDir + File.separator + "assets" + File.separator + "chameleon" + File.separator + "chameleon_splashscreen_0.png"
-        val gameMaleRoleImgPath = decompileDir + File.separator + "assets" + File.separator + "game" + File.separator + "resource" + File.separator + "shj_assets" + File.separator + "picture" + File.separator + "bigbg" + File.separator + "create_role_bg1_f1875cc3.png"
-        val gameFemaleRoleImgPath = decompileDir + File.separator + "assets" + File.separator + "game" + File.separator + "resource" + File.separator + "shj_assets" + File.separator + "picture" + File.separator + "bigbg" + File.separator + "create_role_bg2_e7dbb08c.png"
+        val gameMaleRoleImgPath = decompileDir + File.separator + "assets" + File.separator + "game" + File.separator + "resource" + File.separator + "shj_assets" + File.separator + "picture" + File.separator + "bigbg" + File.separator + "create_role_bg1_ed9d9ee8.png"
+        val gameFemaleRoleImgPath = decompileDir + File.separator + "assets" + File.separator + "game" + File.separator + "resource" + File.separator + "shj_assets" + File.separator + "picture" + File.separator + "bigbg" + File.separator + "create_role_bg2_fc4b58f.png"
         FileUtil.replaceResource(loginImage, gameLoginImgPath)
         FileUtil.replaceResource(splashImage, gameSplashImgPath)
         FileUtil.replaceResource(logoImage, gameMaleRoleImgPath)
