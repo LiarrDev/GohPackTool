d0b3b97b73cc8b39bbcde06c8ee15638d055e9dd
Liarr <liarr@foxmail.com>
2021-06-13 17:44:09 +0800

    Feat: ViVo 联运打包脚本支持 ViVo Union SDK 4.6.0.4 版本

--- src/main/kotlin/goh/utils/AndroidXmlHandler.kt
+++ src/main/kotlin/goh/utils/AndroidXmlHandler.kt
@@ -316,7 +316,7 @@ object AndroidXmlHandler {
                     android:value="$appId" />
                 <meta-data
                     android:name="vivo_union_sdk"
-                    android:value="4.6.0.1" />
+                    android:value="4.6.0.4" />
                 <activity
                     android:name="com.vivo.unionsdk.ui.UnionActivity"
                     android:configChanges="orientation|keyboardHidden|navigation|screenSize"
