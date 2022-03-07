78c73924b9211b04b79f7cb44834f8e036179641
Liarr <liarr@foxmail.com>
2022-03-07 17:06:16 +0800

    Feat: Game 155

--- src/main/kotlin/goh/games/Game155.kt
+++ src/main/kotlin/goh/games/Game155.kt
@@ -7,12 +7,23 @@ import java.io.File
  * GID: 155
  * 九梦仙域
  */
-class Game155(apk: String) : Game(apk)  {
+class Game155(apk: String) : Game(apk) {
     override fun replaceResource(loginImage: String?, loadingImage: String?, logoImage: String?, splashImage: String?) {
-        val gameLoginImgPath = decompileDir + File.separator + "assets" + File.separator + "AgentAssets" + File.separator + "login_bg.png"
-        val gameLoadingImgPath = decompileDir + File.separator + "assets" + File.separator + "AgentAssets" + File.separator + "loading_bg.png"
-        val gameLogoImgPath = decompileDir + File.separator + "assets" + File.separator + "AgentAssets" + File.separator + "logo.png"
-        val gameSplashImgPath = decompileDir + File.separator + "assets" + File.separator + "splash_image_0.png"
+        val gameLoginImgPath = decompileDir + File.separator +
+                "assets" + File.separator +
+                "AgentAssets" + File.separator +
+                "login_bg.png"
+        val gameLoadingImgPath = decompileDir + File.separator +
+                "assets" + File.separator +
+                "AgentAssets" + File.separator +
+                "loading_bg.png"
+        val gameLogoImgPath = decompileDir + File.separator +
+                "assets" + File.separator +
+                "AgentAssets" + File.separator +
+                "logo.png"
+        val gameSplashImgPath = decompileDir + File.separator +
+                "assets" + File.separator +
+                "splash_image_0.png"
         FileUtil.replaceResource(loginImage, gameLoginImgPath)
         FileUtil.replaceResource(loadingImage, gameLoadingImgPath)
         FileUtil.replaceResource(logoImage, gameLogoImgPath)
