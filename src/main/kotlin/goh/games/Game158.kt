2f44d82ecf7576a3e70069402f7c0ef5d127cc70
Liarr <liarr@foxmail.com>
2022-04-21 13:58:28 +0800

    Feat: Game 158

---
+++ src/main/kotlin/goh/games/Game158.kt
@@ -0,0 +1,25 @@
+package goh.games
+
+/**
+ * GID: 158
+ * 逍遥三国
+ */
+class Game158(apk: String) : Game(apk) {
+    override fun replaceResource(
+        loginImage: String?,
+        loadingImage: String?,
+        logoImage: String?,
+        splashImage: String?
+    ) {
+    }
+
+    override fun generateSignedApk(
+        keyStorePath: String,
+        generatePath: String,
+        gid: String,
+        appVersion: String,
+        channelAbbr: String
+    ): Boolean {
+        return generateSignedApk(keyStorePath, generatePath, gid, appVersion, channelAbbr, "jmxy")
+    }
+}
